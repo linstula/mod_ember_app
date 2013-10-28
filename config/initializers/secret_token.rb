f60f71c4195ad4bf61f23b40494eb252e643f225
Lin Reid <linreid@gmail.com>
2013-10-28 09:23:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ModEmberApp::Application.config.secret_key_base = '5bc73bfd2f1539099df1f13d8e129427dd00718201f3315af5c6c3f81ee43c65a3824ea9ccb58247796f34b464c3bdf0ce83af16fccbd841afbbd9115d544f88'
