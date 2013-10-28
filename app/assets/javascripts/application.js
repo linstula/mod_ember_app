8cf17aab416059463de2e7bb38a9c53792f25d71
Lin Reid <linreid@gmail.com>
2013-10-28 09:25:29 -0400

    run ember:bootstrap with coffee

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require mod_ember_app
+
+// for more details see: http://emberjs.com/guides/application/
+ModEmberApp = Ember.Application.create();
+
 //= require_tree .
