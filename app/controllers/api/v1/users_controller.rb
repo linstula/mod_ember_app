966538c294e64441ff36a0165c5f648e40a949a5
Lin Reid <linreid@gmail.com>
2013-10-29 11:55:25 -0400

    add show action and route for api::v1::users_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,4 +2,9 @@ class Api::V1::UsersController < ApplicationController
   def index
     render json: User.all
   end
+
+  # Don't know if this show action is required. Inspector console shows an error if not present
+  def show
+    render json: User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ModEmberApp::Application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index]
+      resources :users, only: [:index, :show]
     end
   end
 
